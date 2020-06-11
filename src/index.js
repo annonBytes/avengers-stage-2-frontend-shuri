cae116ba1e2da770d54137d9e864f71e585de83a
annonBytes@gmail.com <beinmonyudaniel@gmail.com>
2020-06-11 04:00:15 +0100

    Page structure and styling

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+$('.carousel-testimonial').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
